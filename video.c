8a54fe801beeebaeb1b6b16ac2edccaa5195d369
Cheurfa Hanna Sanna <hannasannacheurfa@icloud.com>
2024-11-23 20:50:53 +0100

    msg

--- video.c
+++ video.c
@@ -11,29 +11,28 @@
 // Return value
 //   colored video size (based on the unit passed parameter)
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
-    // Bits per pixel for colored image (24 bits)
-    float bitsPerPixel = 24;
+    // Bits per pixel for colored image (24 bits)
+     float bitsPerPixel = 24;
 
-    float clrImage = w * h * bitsPerPixel * durationMovie * fps;
-    float BImage = w * h * durationCredits * fps;
-    float sizeInBits = clrImage + BImage; // Add to total size in bits
-    float size;
+float clrImage = w * h * bitsPerPixel * durationMovie * fps;
+float BImage = w * h * durationCredits * fps;
+float sizeInBits = clrImage + BImage; // Add to total size in bits
+float size;
 
 
 
-    // Convert size based on the requested unit
-    if (strcmp(unit, "bt") == 0) {
-        size = sizeInBits; // Convert to bytes
-    } else if (strcmp(unit, "ko") == 0) {
-        size = sizeInBits / (1024); // Convert to kilobits
-    } else if (strcmp(unit, "mo") == 0) {
-        size = sizeInBits / (1024 * 1024); // Convert to megabits
-    } else if (strcmp(unit, "go") == 0) {
-        size = sizeInBits / (1024 * 1024 * 1024); // Convert to gigabits
-    } else {
-        // If the unit is not recognized, return -1 or some error value
-        return -1.0f;
-    }
-
-   return size / 8;
+// Convert size based on the requested unit
+if (strcmp(unit, "bt") == 0) {
+size = sizeInBits; // Convert to bytes
+} else if (strcmp(unit, "ko") == 0) {
+size = sizeInBits / (1024); // Convert to kilobits
+} else if (strcmp(unit, "mo") == 0) {
+size = sizeInBits / (1024 * 1024); // Convert to megabits
+} else if (strcmp(unit, "go") == 0) {
+size = sizeInBits / (1024 * 1024 * 1024); // Convert to gigabits
+} else {
+// If the unit is not recognized, return -1 or some error value
+return -1.0f;
 }
+
+return size / 8;}
